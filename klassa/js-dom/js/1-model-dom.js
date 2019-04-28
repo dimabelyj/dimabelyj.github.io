6c0394c48c2454255b19ce7481655d83066929db
Dmytro Bilokolodskykh <belokolodskikh@gmail.com>
2019-04-28 09:29:42 +0200

    done

---
+++ klassa/js-dom/js/1-model-dom.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// glówny objekt w dokumentu HTML
+console.log(window);
+// do sliedzenia powiekszenia ekranu skrolowania i t.d.
+console.log(window.innerHeight);
+console.log(window.outerHeight);
+
+// pobranie elementu po id. klasie
+console.log(document);
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
