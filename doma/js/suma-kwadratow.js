3e868dc30fc50f53dbb797fbe20e41d30eb0d1a7
Dmytro Bilokolodskykh <belokolodskikh@gmail.com>
2019-05-02 19:38:13 +0200

    done

--- doma/js/suma-kwadratow.js
+++ doma/js/suma-kwadratow.js
@@ -5,6 +5,6 @@ const mojaFunkcja = () => {
     for (let value of iter) {
         value = Math.sqrt(iter[0]) + Math.sqrt(iter[1]) + Math.sqrt(iter[2]) + Math.sqrt(iter[3]) + Math.sqrt(iter[4]) + Math.sqrt(iter[5]);
         console.log(value);
-        // console.log(value1);
     }
 }
+mojaFunkcja();
