7dd3faf7a83e0f707e904c8c8352fea49ada4418
Dmytro Bilokolodskykh <belokolodskikh@gmail.com>
2019-05-10 22:44:04 +0200

    done

--- doma/js/objekty-ksiazka.js
+++ doma/js/objekty-ksiazka.js
@@ -14,28 +14,34 @@ class Ksiazka {
     }
 
 }
-let ksiazka1 = new Ksiazka('The Old Man and the Sea', 'Hemingway', true);
-ksiazka1.opiszKsiazke();
+let ksiazka1 = new Ksiazka('The Old Man and the Sea', 'Hemingway', false);
+// ksiazka1.opiszKsiazke();
 
-let ksiazka2 = new Ksiazka('Баязет', 'Пикуль', false);
-ksiazka2.opiszKsiazke();
+let ksiazka2 = new Ksiazka('Баязет', 'Пикуль', true);
+// ksiazka2.opiszKsiazke();
 
-let ksiazka3 = new Ksiazka('Мастер и Маргарита', 'Булгаков', false);
-ksiazka3.opiszKsiazke();
+let ksiazka3 = new Ksiazka('Мастер и Маргарита', 'Булгаков', true);
+// ksiazka3.opiszKsiazke();
 
 let books = [ksiazka1, ksiazka2, ksiazka3];
 console.log(books);
 
 const iloscPrzeczytanych = (books) => {
     let countReaded = 0;
+    let countNotReaded = 0;
     for (let i = 0; i < books.length; i++) {
         let book = books[i];
         if (book._przeczytana == true) {
             countReaded += 1;
+            book.opiszKsiazke();
+        } else if (book._przeczytana == false) {
+            countNotReaded += 1;
+            book.opiszKsiazke();
         }
 
     }
-    console.log(countReaded);
+    console.log('ilosc przeczytanych = ' + countReaded);
+    console.log('ilosc nie przeczytanych = ' + countNotReaded);
 }
 
 
