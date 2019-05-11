23b0976a0a2a48b4011c20d918833d55019f11af
Dmytro Bilokolodskykh <belokolodskikh@gmail.com>
2019-05-11 09:55:55 +0200

    done

---
+++ klassa/JS jQuery/js/2-selektory.js
@@ -0,0 +1,11 @@
+'use strict';
+let naglowe1 = $('#naglowek-1');
+console.log(naglowe1);
+
+naglowe1.css('color', 'red');
+
+let paragrafy = $('p');
+console.log(paragrafy);
+
+let paragrafyByClass = $('.par');
+console.log(paragrafyByClass);
