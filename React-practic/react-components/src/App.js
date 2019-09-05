31ea3d988c23947f98752c83d3e5015a56dedae1
Dima <belokolodskikh@gmail.com>
2019-09-05 20:15:05 +0200

    passing param lesson1

--- React-practic/react-components/src/App.js
+++ React-practic/react-components/src/App.js
@@ -3,29 +3,36 @@ import './App.css';
 import PassParamToComponent from './PassParamToComponent/PassParamToComponent';
 
 class App extends Component {
-  state = {
-    cars: [
-      { name: 'Ford', year: 2018 },
-      { name: 'Audi', year: 2016 },
-      { name: 'Mazda', year: 2010 }
-    ],
-    pageTitle: 'React component through "state"',
-    showCars: false
+
+  constructor(props) {
+    console.log('App constructor');
+    super(props)
+
+    this.state ={
+      cars: [
+        { name: 'Ford', year: 2018 },
+        // { name: 'Audi', year: 2016 },
+        // { name: 'Mazda', year: 2010 }
+      ],
+      pageTitle: 'React component through "state"',
+      showCars: false
+    }
   }
 
-  onChangeName(name, index){
+
+  onChangeName(name, index) {
     const car = this.state.cars[index];
     car.name = name;
     const cars = [...this.state.cars];
     cars[index] = car;
-    this.setState({cars});
+    this.setState({ cars });
   }
 
-  deleteHandler(index){
+  deleteHandler(index) {
     const cars = this.state.cars.concat();
 
     cars.splice(index, 1);
-    this.setState({cars});
+    this.setState({ cars });
   }
 
   changeInput = (event) => {
@@ -42,39 +49,50 @@ class App extends Component {
     })
   }
 
-  render() {
+  componentWillMount() {
+    console.log('App componentWillMount');
+  }
 
+  componentDidMount(){
+    console.log('App componentDidMount');
+  }
+
+
+
+  render() {
+    console.log('App render');
     const divStyle = {
       'textAlign': 'center'
     };
 
     return (
       <div style={divStyle}>
-        <h1 style={{ backgroundColor: 'green', color: 'blue' }}>{this.state.pageTitle}</h1>
-        <input type="text" onChange={this.changeInput} />
+        {/* <h1 style={{ backgroundColor: 'green', color: 'blue' }}>{this.state.pageTitle}</h1> */}
+        <h1>{this.props.title}</h1>
+        {/* <input type="text" onChange={this.changeInput} /> */}
         <button
           onClick={this.toggleCarsHandler}
         >Toggle cars</button>
-        
+
         {this.state.showCars
           ? this.state.cars.map((car, index) => {
             return (
-              <div style = {{
+              <div style={{
                 width: 400,
                 margin: 'auto',
                 paddingTop: '20px'
               }}>
-              <PassParamToComponent
-                key={index}
-                name={car.name}
-                year={car.year}
-                onChangeTitle={() => this.changeTitleHandler(car.name)}
-                onChangeName={event => this.onChangeName(event.target.value, index)}
-                onDelete = {this.deleteHandler.bind(this, index)}
-              />
+                <PassParamToComponent
+                  key={index}
+                  name={car.name}
+                  year={car.year}
+                  onChangeTitle={() => this.changeTitleHandler(car.name)}
+                  onChangeName={event => this.onChangeName(event.target.value, index)}
+                  onDelete={this.deleteHandler.bind(this, index)}
+                />
               </div>
             )
-          }) 
+          })
           : null
         }
       </div>
--- React-practic/react-components/src/PassParamToComponent/PassParamToComponent.js
+++ React-practic/react-components/src/PassParamToComponent/PassParamToComponent.js
@@ -1,31 +1,53 @@
 import React from 'react';
 import './PassParamToComponent.css'
 
-export default param => {
+class PassParamToComponent extends React.Component{
 
-  const inputClasses = ['input'];
-
-  if (param.name !== ''){
-    inputClasses.push('green');
-  } else {
-    inputClasses.push('red');
+  componentWillReceiveProps(nextProps){
+    console.log('PassParamToComponent componentWillReceiveProps', nextProps);
   }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('PassParamToComponent shouldComponentUpdate', nextProps, nextState);
+    return nextProps.name.trim() !== this.props.name.trim();
+  }
+
+  componentWillUpdate(nextProps, nextState){
+    console.log('PassParamToComponent componentWillUpdate', nextProps, nextState);
+  }
+
+  componentDidUpdate(){
+    console.log('PassParamToComponent componentDidUpdate');
+  }
+
+  render(){
+    console.log('PassParamToComponent render');
+    const inputClasses = ['input'];
+
+    if (this.props.name !== ''){
+      inputClasses.push('green');
+    } else {
+      inputClasses.push('red');
+    }
+    
+    if (this.props.name.length > 4){
+      inputClasses.push('bold');
+    }
   
-  if (param.name.length > 4){
-    inputClasses.push('bold');
+    return (
+      <div className="PassParamToComponent">
+        <h3>Car name: {this.props.name}</h3>
+        <p>Year: <strong>{this.props.year}</strong></p>
+        <input
+          type="text"
+          onChange = {this.props.onChangeName}
+          defaultValue={this.props.name}
+          className = {inputClasses.join(' ')}
+        />
+        <button onClick={this.props.onDelete}>Delete</button>
+      </div>
+    )
   }
-
-  return (
-    <div className="PassParamToComponent">
-      <h3>Car name: {param.name}</h3>
-      <p>Year: <strong>{param.year}</strong></p>
-      <input
-        type="text"
-        onChange = {param.onChangeName}
-        defaultValue={param.name}
-        className = {inputClasses.join(' ')}
-      />
-      <button onClick={param.onDelete}>Delete</button>
-    </div>
-  )
 }
+
+export default PassParamToComponent
